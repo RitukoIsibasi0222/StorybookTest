d97a7ed317ac217d74f238039d5fb5d61bae0235
RitukoIsibasi0222 <ritukodayo40@gmail.com>
2025-01-04 12:41:11 +0900

    .storybook/mainjs  a11y表記の修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,20 @@
 /** @type { import('@storybook/svelte-vite').StorybookConfig } */
 const config = {
-  stories: [
-    '../src/**/*.stories.js',
-    '../src/**/*.stories.svelte',
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
 
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@chromatic-com/storybook",
     '@storybook/addon-a11y',
   ],
 
   staticDirs: ["../public"],
 
   framework: {
-    name: '@storybook/svelte-vite',
+    name: "@storybook/svelte-vite",
     options: {},
   },
 
