ab32a0e5ba3fcdf877dfc9c74fdaa98d2bc27e13
RitukoIsibasi0222 <ritukodayo40@gmail.com>
2025-01-04 21:20:45 +0900

    squareコンポーネントを作成

---
+++ src/component/square/square.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let width = 120;
+    export let height = 120;
+    export let color = '#cccccc';
+</script>
+
+<style>
+    .square {
+        width: var(--width);
+        height: var(--height);
+        background-color: var(--color);
+    }
+</style>
+
+<div
+    class="square"
+    style="--width: {width}px; --height: {height}px; --color: {color};">   
+</div>
